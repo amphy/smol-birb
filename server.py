6a46b13a6230a63fb9e7e003c728225f172f337f
Lord and Master of All Things Big and Small <amphy.kit@gmail.com>
2019-04-05 17:42:11 -0500

    testing match history

--- server.py
+++ server.py
@@ -1,17 +1,24 @@
-from flask import Flask
-import cassiopeia as cass
 import os
+
+from flask import Flask
+
+import cassiopeia as cass
+from cassiopeia import Summoner, Match
+from cassiopeia.data import Season, Queue
 
 app = Flask(__name__)
 
 @app.route('/')
 def display_build():
 	# Get summoner
+	summoner = Summoner(name="Olysia", region="NA")
 	# Get Match
+	match_history = summoner.match_history
+
 	# Get items for champion from Match
 	# display items and champion picture on page
 
-    return 'Hello, World!'
+    return match_history
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
