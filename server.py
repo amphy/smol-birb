e3cd30769ac41fb96ecd462f64b519a8c4a491f9
Lord and Master of All Things Big and Small <amphy.kit@gmail.com>
2019-04-05 17:49:51 -0500

    added some more code for cass stuff

--- server.py
+++ server.py
@@ -10,12 +10,22 @@ app = Flask(__name__)
 
 @app.route('/')
 def display_build():
+	region = "NA"
+	summoner_name = "Olysia"
+
+	cass.set_riot_api_key(os.environ.get("RIOTAPI_key", "None"))
+
+	champion_id_to_name_mapping = {champion.id: champion.name for champion in cass.get_champions(region=region)}
+
 	# Get summoner
-	summoner = Summoner(name="Olysia", region="NA")
+	summoner = Summoner(name=summoner_name, region=region)
 	# Get Match
 	match_history = summoner.match_history
-
+	match = match_history[0]
 	# Get items for champion from Match
+	champion_id = match.participants[summoner.name].champion.champion.id
+	champion_name = champion_id_to_name_mapping[champion_id]
+
 	# display items and champion picture on page
 
     return match_history
