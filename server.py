9a3e099714a4c8368b8a4f6a53efc8bf309db797
Lord and Master of All Things Big and Small <amphy.kit@gmail.com>
2019-04-05 19:17:38 -0500

    finished now need readme

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, request
 
 import cassiopeia as cass
 from cassiopeia import Summoner, Match
@@ -9,9 +9,16 @@ from cassiopeia.data import Season, Queue
 app = Flask(__name__)
 
 @app.route('/')
-def display_build():
+def homepage():
+	return render_template('index.html')
+
+@app.route('/results', methods = ['GET'])
+def results():
 	region = "NA"
-	summoner_name = "Olysia"
+	try:
+		summoner_name = str(request.args.getlist('search')[0])
+	except:
+		summoner_name = "Olysia"
 
 	cass.set_riot_api_key(os.environ.get("RIOTAPI_KEY", "None"))
 
@@ -23,12 +30,24 @@ def display_build():
 	match_history = summoner.match_history
 	match = match_history[0]
 	# Get items for champion from Match
-	champion_id = match.participants[summoner.name].champion.champion.id
+	champion_id = match.participants[summoner.name].champion.id
+	champ_img = match.participants[summoner.name].champion.image.url
 	champion_name = champion_id_to_name_mapping[champion_id]
+	items = match.participants[summoner.name].stats.items
+
+	itemUrls = []
+	for item in items:
+		# We want to get picture
+		if item is not None:
+			print(item.name)
+			itemUrl = item.image.url
+			itemUrls.append(itemUrl)
+		else:
+			itemUrl = 'static/none.png'
+			itemUrls.append(itemUrl)
 
 	# display items and champion picture on page
-
-    return match_history
+	return render_template('results.html', url_champ = champ_img, champ_name = champion_name, url_img1 = itemUrls[0], url_img2 = itemUrls[1], url_img3 = itemUrls[2], url_img4 = itemUrls[3], url_img5 = itemUrls[4], url_img6 = itemUrls[5])
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
